50997078fff31cd9e89d6867afdb3332d307582d
deletfsi <wzltdi@gmail.com>
2026-05-21 11:39:27 +0800

    Optimize mobile archive experience

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['./index_test.html'],
+  theme: {
+    extend: {
+      colors: {
+        miku: '#39C5BB',
+        'miku-dark': '#1e6a64',
+        'miku-glow': 'rgba(57, 197, 187, 0.5)',
+        dark: '#03070b',
+        surface: 'rgba(255, 255, 255, 0.03)',
+        surfaceHover: 'rgba(255, 255, 255, 0.08)',
+        glass: 'rgba(10, 15, 20, 0.6)'
+      },
+      fontFamily: {
+        sans: ['"PingFang SC"', '"Microsoft YaHei"', 'sans-serif'],
+        display: ['"Space Grotesk"', 'sans-serif']
+      }
+    }
+  }
+};
